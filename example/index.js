fc53be0478ff9627518f609c7f84b3557c05a0be
pjnalls <preston.james.nalls@gmail.com>
2021-01-10 08:46:02 -0600

    Add fs module to read UI bootstrapping.

--- example/index.js
+++ example/index.js
@@ -1,13 +1,14 @@
+const fs = require('fs');
+
 exports.index = `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Unsweet.js Sugar Journal</title>
-  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   <div id="root"></div>
-  <script src='app.js'></script>
+  <script>${fs.readFileSync('./ui.js')}</script>
 </body>
 </html>`;
