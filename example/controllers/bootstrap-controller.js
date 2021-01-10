b22d7b678ae50d2d69a88fa28e424ad1b00bcaab
pjnalls <preston.james.nalls@gmail.com>
2021-01-10 08:47:50 -0600

    Make minor edit.

--- example/controllers/bootstrap-controller.js
+++ example/controllers/bootstrap-controller.js
@@ -1,6 +1,6 @@
-const Index = require('../index');
+const { index } = require('../index');
 
 exports.bootstrapApp = (req, res) => {
-  try { res.writeHead(200, { 'Content-Type' : 'text/html' }).end(Index); }
+  try { res.writeHead(200, { 'Content-Type' : 'text/html' }).end(index); }
   catch (error) { console.log(error); }
 }
