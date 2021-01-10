dd080f88a112189ea2990ce078091e11b49c38ab
pjnalls <preston.james.nalls@gmail.com>
2021-01-10 08:47:23 -0600

    Add UI-bootstapping for now.

--- example/server.js
+++ example/server.js
@@ -1,17 +1,8 @@
 const http = require('http');
 const { bootstrapApp } = require('./controllers/bootstrap-controller');
-const { getAllFoods } = require('./controllers/food-controller');
 
-// TODO: Modularize web APIs.
 const server = http.createServer((req, res) => {
-  bootstrapApp();
-
-  if (req.url === '/api/foods' && req.method === 'GET') {
-    getAllFoods();
-  } else {
-    res.writeHead(404, { 'Content-Type' : 'application/json' });
-    res.end(JSON.stringify({ message: 'Route Not Found'}));
-  }
+  bootstrapApp(req, res);
 });
 
 const PORT = process.env.PORT || 5000;
