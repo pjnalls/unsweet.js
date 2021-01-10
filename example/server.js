882a4a52782fad3e2dbbbd991c27663ca9742514
pjnalls <preston.james.nalls@gmail.com>
2021-01-10 13:59:20 -0600

    Add API to get all foods.

--- example/server.js
+++ example/server.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const { bootstrapApp } = require('./controllers/bootstrap-controller');
+const { getAllFoods } = require('./controllers/food-controller');
 
 const server = http.createServer((req, res) => {
-  bootstrapApp(req, res);
+  if (req.url === '/' && req.method === 'GET') bootstrapApp(req, res);
+  else if (req.url === '/api/foods' && req.method === 'GET') getAllFoods(req, res);
 });
 
 const PORT = process.env.PORT || 5000;
